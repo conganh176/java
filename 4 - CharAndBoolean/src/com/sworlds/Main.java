9462336e8bc2445c3a7312c1e460cab650de9a7d
Cong Anh Nguyen <sworlds@Congs-MacBook-Air.local>
2019-03-13 13:41:07 +0700

    java

---
+++ 4 - CharAndBoolean/src/com/sworlds/Main.java
@@ -0,0 +1,13 @@
+package com.sworlds;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //width of 16 (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println("myChar = " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+    }
+}
