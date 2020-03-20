618ec48fefe0d48b5d48568c80aee3c0b998ffe7
Cong Anh Nguyen <sworlds@Congs-MacBook-Air.local>
2020-03-20 22:10:51 +0700

    modify

--- 12-While/src/com/company/Main.java
+++ 12-While/src/com/company/Main.java
@@ -3,6 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int count = 0;
+
+        System.out.println("While\n");
+        while (count < 10)
+        {
+            System.out.println(count);
+            count++;
+        }
+
+        System.out.println("Do\n");
+        do
+        {
+            System.out.println(count);
+            count--;
+        } while (count > 0);
+
+        int num = 0;
+        System.out.println("Function\n");
+        while (count <= 100)
+        {
+            count ++;
+            if (count % 2 != 0)
+            {
+                continue;
+            }
+            num += count;
+        }
+
+        System.out.println("Total: " + num);
     }
 }
